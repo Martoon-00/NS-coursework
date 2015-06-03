6233baebf8b65687b1979c7fa67faf7e6224bfff
Koshik <elena@localhost.localdomain>
2015-06-03 21:18:50 +0300

    :ok:

--- src/ru/ifmo/graphics/Graphics.java
+++ src/ru/ifmo/graphics/Graphics.java
@@ -1,23 +1,42 @@
 package ru.ifmo.graphics;
 
-import com.xeiam.xchart.Chart;
-import com.xeiam.xchart.QuickChart;
-import com.xeiam.xchart.SwingWrapper;
+import com.xeiam.xchart.*;
+import ru.ifmo.lang.Typke;
+
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class Graphics {
 
 
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-            double[] xData = new double[] { 0.0, 1.0, 2.0 };
-            double[] yData = new double[] { 2.0, 1.0, 0.0 };
+        double[] xData = new double[]{0.0, 1.0, 2.0};
+        double[] yData = new double[]{2.0, 1.0, 0.0};
 
-            // Create Chart
-            Chart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+        LinkedList<Typke> list = new LinkedList<>();
+        list.add(new Typke(2,2));
+        list.add(new Typke(4,4));
+        list.add(new Typke(100, 4));
+        drawLnInv(list);
 
-            // Show it
-            new SwingWrapper(chart).displayChart();
+    }
 
+    public static void drawLnInv(LinkedList<Typke> ... functions) {
+        Chart chart = new ChartBuilder().width(800).height(600).build();
+        chart.getStyleManager().setYAxisLogarithmic(true);
+        chart.getStyleManager().setChartTitleVisible(false);
+        chart.getStyleManager().setLegendPosition(StyleManager.LegendPosition.InsideNW);
+        for (LinkedList<Typke> function : functions) {
+            List<Double> xData = new LinkedList<>();
+            List<Double> yData = new LinkedList<>();
+            for (Typke point : function) {
+                xData.add(point.x);
+                yData.add(point.y);
+            }
+            chart.addSeries("a",xData, yData);
         }
+        new SwingWrapper(chart).displayChart();
+    }
 }
