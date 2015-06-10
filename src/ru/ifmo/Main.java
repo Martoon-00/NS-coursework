e51471aaa3fc3d2730184a969d08cc6f746589bd
Martoon <Martoon_00@mail.ru>
2015-06-10 03:49:31 +0300

    Titles fixed

--- src/ru/ifmo/Main.java
+++ src/ru/ifmo/Main.java
@@ -14,7 +14,7 @@ import static ru.ifmo.lang.Experiment.ExperimentSeries;
 
 public class Main {
     public static void main(String[] args) {
-        analyzeEqSystem3(0.1);
+        analyzeEqSystem3(0.);
     }
 
     public static void analyzeEqSystem1() {
@@ -26,10 +26,10 @@ public class Main {
 
         BiFunction<Integer, Double, Double> countG = (index, t) -> -solver.apply(t).get(index) / R / t / sigma * Coefficients.getDCounter("AlCl" + (index == 0 ? "" : (index + 1) + "")).apply(t);
 
-        new Graphics("T^-1", "ln V_Al")
+        new Graphics("T^-1", "ln -V_Al")
                 .addGraphic(experiments.create(t -> Arrays.asList(0, 1, 2).stream().map(index -> countG.apply(index, t)).reduce(0., (a, b) -> a + b) * Coefficients.getMolarVolume("Al") * 1e9).get(), "V_Al")
                 .show();
-        new Graphics("T^-1", "ln G")
+        new Graphics("T^-1", "ln -G")
                 .addGraphic(experiments.create(t -> countG.apply(0, t)).get(), "AlCl")
                 .addGraphic(experiments.create(t -> countG.apply(1, t)).get(), "AlCl2")
                 .addGraphic(experiments.create(t -> countG.apply(2, t)).get(), "AlCl3")
@@ -46,10 +46,10 @@ public class Main {
 
         BiFunction<Integer, Double, Double> countG = (index, t) -> -solver.apply(t).get(index) / R / t / sigma * Coefficients.getDCounter("GaCl" + (index == 0 ? "" : (index + 1) + "")).apply(t);
 
-        new Graphics("T^-1", "ln V_Ga")
+        new Graphics("T^-1", "ln -V_Ga")
                 .addGraphic(experiments.create(t -> Arrays.asList(0, 1, 2).stream().map(index -> countG.apply(index, t)).reduce(0., (a, b) -> a + b) * Coefficients.getMolarVolume("Ga") * 1e9).get(), "V_Ga")
                 .show();
-        new Graphics("T^-1", "ln G")
+        new Graphics("T^-1", "ln -G")
                 .addGraphic(experiments.create(t -> countG.apply(0, t)).get(), "GaCl")
                 .addGraphic(experiments.create(t -> countG.apply(1, t)).get(), "GaCl2")
                 .addGraphic(experiments.create(t -> countG.apply(2, t)).get(), "GaCl3")
